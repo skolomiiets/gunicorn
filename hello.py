48689610e7497c1396c88b7cc5326613370254ea
iptoname <iptoname@gmail.com>
2016-02-28 20:22:39 +0000

    new configs

--- hello.py
+++ hello.py
@@ -1,11 +1,11 @@
 import urlparse
 
 def app(env, start_response):
-    query = QUERY_STRING
-    body = urlparse.parse_qs(query, keep_blank_values=True)
-    for i, j in params.items():
-        return str(str(i) + '=' + str(j))\n
+#    query = env[QUERY_STRING]
+    body = urlparse.parse_qs(env['QUERY_STRING'], keep_blank_values=True)
+#    for i, j in params.items():
+#        return str(str(i) + '=' + str(j))\n
     status = '200 OK'
     headers = [('Content-Type', 'text/plain')]
     start_response(status, headers)
-    return  [ body ]
+    return  iter([ body ])
